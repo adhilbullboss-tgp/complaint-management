28293a0c9580423357ee4945057d3a7a6e44a3f8
adhilbullboss-tgp <adhilbullboss@gmail.com>
2026-06-21 01:24:13 -0700

    Update DBconnection.java

--- database/DBconnection.java
+++ database/DBconnection.java
@@ -1,1 +1,27 @@
+package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+        Connection con = null;
+
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/managementofcomplaint_schema",
+                "root","adhil_123" );
+
+            System.out.println("Connected!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+}
